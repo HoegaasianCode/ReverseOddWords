8103235abf675f38a823956a0f0f7e019f93d838
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-18 13:21:08 +0100

    Initial commit

---
+++ ReversedOddLengthCases/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReversedOddLengthCases
+{
+    class Program
+    {
+        // https://edabit.com/challenge/ny83khdmye4JsJRqS
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var x = new Reverser("One two three four");
+            Console.WriteLine(x.ParseSentence());
+        }
+    }
+}
---
+++ ReversedOddLengthCases/Reverser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedOddLengthCases
+{
+    public class Reverser
+    {
+        private readonly string _string;
+
+        public Reverser(string @string)
+        {
+            _string = @string;
+        }
+
+        public string ParseSentence()
+        {
+            string[] words = _string.Split(" ");
+            var sb = new StringBuilder();
+            foreach(var word in words)
+            {
+                if (word.Length % 2 == 0)
+                {
+                    sb.Append(word);
+                    sb.Append(' ');
+                }
+                else
+                {
+                    sb.Append(word.Reverse().ToArray());
+                    sb.Append(' ');
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
